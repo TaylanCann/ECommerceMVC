4d14a95932ee377a95b6962203a3d966b8a7c8c8
TaylanCann <taylancanh@gmail.com>
2022-05-02 20:28:14 +0300

    Categories are listing

--- ECommerceMVC/ViewComponents/MenuViewComponent.cs
+++ ECommerceMVC/ViewComponents/MenuViewComponent.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceMVC.Business.IServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceMVC.ViewComponents
 {
     public class MenuViewComponent : ViewComponent
     {
-
+        private readonly ICategoryService categoryService;
+        public MenuViewComponent(ICategoryService categoryService) 
+        {
+            this.categoryService = categoryService;
+        }
+        public IViewComponentResult Invoke() 
+        {
+            var categories = categoryService.GetCategories();
+            return View(categories);
+        }
     }
 }
