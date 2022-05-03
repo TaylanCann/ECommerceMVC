55e9a5950ec3d8425032b5046f859111d74ffcf8
TaylanCann <taylancanh@gmail.com>
2022-05-03 18:00:50 +0300

    AddDbDatas

--- ECommerceMVC.DataAccess/Data/ECommerceDbContext.cs
+++ ECommerceMVC.DataAccess/Data/ECommerceDbContext.cs
@@ -28,13 +28,81 @@ namespace ECommerceMVC.DataAccess.Data
                     {
                         new Product
                         {
-                            Id = 1,
-                            Name = "iPhone",
-                            
+                            Id = 1, 
+                            Name = "iPhone 11", 
+                            Price=7000,
+                            Discount=0.02,
+                            Description="Awasome",
+                            ImageURL="https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-2_large.jpg"
+                        },
+                        
+                        new Product
+                        {
+                            Id = 2,
+                            Name = "Samsung",
+                            Price=7000,
+                            Discount=0.02,
+                            Description="Cool",
+                            ImageURL="https://productimages.hepsiburada.net/s/44/550/10769576230962.jpg/format:webp"
+
+                        },
+
+                         new Product
+                        {
+                            Id = 3,
+                            Name = "Xiomi",
+                            Price=7000,
+                            Discount=0.02,
+                            Description="Chang Ching Chong",
+                            ImageURL="https://productimages.hepsiburada.net/s/198/550/110000168630735.jpg/format:webp"
 
                         }
                     }
-                }
+                },
+
+                 new Category
+                 {
+                     Id = 2,
+                     Name = "Computer",
+                     Products = new List<Product>
+                    {
+                        new Product
+                        {
+                            Id = 4, 
+                            Name = "MacBook Pro1",
+                            Price=20000,
+                            Discount=0.09,
+                            Description="Interesting", 
+                            ImageURL="https://productimages.hepsiburada.net/s/44/222-222/10817626898482.jpg/format:webp"
+                        },
+
+                        new Product
+                        {
+                            Id = 2,
+                            Name = "Casper",
+                            Price=7000,
+                            Discount=0.02,
+                            Description="Ghost",
+                            ImageURL="https://productimages.hepsiburada.net/s/203/550/110000176911589.jpg/format:webp"
+
+                        },
+
+                         new Product
+                        {
+                            Id = 3,
+                            Name = "monster",
+                            Price=7000,
+                            Discount=0.02,
+                            Description="Canavar",
+                            ImageURL="https://productimages.hepsiburada.net/s/108/550/110000052006351.jpg/format:webp"
+
+                        }
+                    },
+                 },
+
+                 new Category {Id=3,Name="Tablet" }
+
+
             );
         }
     }
