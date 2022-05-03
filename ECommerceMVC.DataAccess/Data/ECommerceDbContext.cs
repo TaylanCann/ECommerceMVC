1043d01f07dd45a4f782b6287f0c3a2e70312559
TaylanCann <taylancanh@gmail.com>
2022-05-03 18:37:34 +0300

    First Migration

--- ECommerceMVC.DataAccess/Data/ECommerceDbContext.cs
+++ ECommerceMVC.DataAccess/Data/ECommerceDbContext.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace ECommerceMVC.DataAccess.Data
 {
-    public class ECommerceDbContext :DbContext
+    public class ECommerceDbContext : DbContext
     {
-        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options) 
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
         {
 
         }
@@ -23,91 +23,86 @@ namespace ECommerceMVC.DataAccess.Data
                                           .WithMany(c => c.Products)
                                           .HasForeignKey(p => p.CategoryId)
                                           .OnDelete(DeleteBehavior.NoAction);
+
             modelBuilder.Entity<Category>().HasData(
 
-                new Category
+                new Category {  Id = 1, Name = "Phone", },
+
+                new Category { Id = 2, Name = "Computer",},
+
+                new Category { Id = 3, Name = "Tablet" }
+
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
                 {
                     Id = 1,
-                    Name = "Phone",
-                    Products = new List<Product>
-                    {
-                        new Product
-                        {
-                            Id = 1, 
-                            Name = "iPhone 11", 
-                            Price=7000,
-                            Discount=0.02,
-                            Description="Awasome",
-                            ImageURL="https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-2_large.jpg"
-                        },
-                        
-                        new Product
-                        {
-                            Id = 2,
-                            Name = "Samsung",
-                            Price=7000,
-                            Discount=0.02,
-                            Description="Cool",
-                            ImageURL="https://productimages.hepsiburada.net/s/44/550/10769576230962.jpg/format:webp"
-
-                        },
-
-                         new Product
-                        {
-                            Id = 3,
-                            Name = "Xiomi",
-                            Price=7000,
-                            Discount=0.02,
-                            Description="Chang Ching Chong",
-                            ImageURL="https://productimages.hepsiburada.net/s/198/550/110000168630735.jpg/format:webp"
-
-                        }
-                    }
+                    Name = "iPhone 11",
+                    CategoryId = 1,
+                    Price = 7000,
+                    Discount = 0.02,
+                    Description = "Awasome",
+                    ImageURL = "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-2_large.jpg"
                 },
 
-                 new Category
-                 {
-                     Id = 2,
-                     Name = "Computer",
-                     Products = new List<Product>
-                    {
-                        new Product
-                        {
-                            Id = 4, 
-                            Name = "MacBook Pro1",
-                            Price=20000,
-                            Discount=0.09,
-                            Description="Interesting", 
-                            ImageURL="https://productimages.hepsiburada.net/s/44/222-222/10817626898482.jpg/format:webp"
-                        },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Samsung",
+                    CategoryId = 1,
+                    Price = 7000,
+                    Discount = 0.02,
+                    Description = "Cool",
+                    ImageURL = "https://productimages.hepsiburada.net/s/44/550/10769576230962.jpg/format:webp"
 
-                        new Product
-                        {
-                            Id = 2,
-                            Name = "Casper",
-                            Price=7000,
-                            Discount=0.02,
-                            Description="Ghost",
-                            ImageURL="https://productimages.hepsiburada.net/s/203/550/110000176911589.jpg/format:webp"
+                },
 
-                        },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Xiomi",
+                    CategoryId = 1,
+                    Price = 7000,
+                    Discount = 0.02,
+                    Description = "Chang Ching Chong",
+                    ImageURL = "https://productimages.hepsiburada.net/s/198/550/110000168630735.jpg/format:webp"
 
-                         new Product
-                        {
-                            Id = 3,
-                            Name = "monster",
-                            Price=7000,
-                            Discount=0.02,
-                            Description="Canavar",
-                            ImageURL="https://productimages.hepsiburada.net/s/108/550/110000052006351.jpg/format:webp"
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "MacBook Pro1",
+                    CategoryId = 2,
+                    Price = 20000,
+                    Discount = 0.09,
+                    Description = "Interesting",
+                    ImageURL = "https://productimages.hepsiburada.net/s/44/222-222/10817626898482.jpg/format:webp"
+                },
 
-                        }
-                    },
-                 },
+                new Product
+                {
+                    Id = 5,
+                    Name = "Casper",
+                    CategoryId = 2,
+                    Price = 7000,
+                    Discount = 0.02,
+                    Description = "Ghost",
+                    ImageURL = "https://productimages.hepsiburada.net/s/203/550/110000176911589.jpg/format:webp"
 
-                 new Category {Id=3,Name="Tablet" }
+                },
 
+                new Product
+                {
+                    Id = 6,
+                    Name = "Monster",
+                    CategoryId = 2,
+                    Price = 7000,
+                    Discount = 0.02,
+                    Description = "Canavar",
+                    ImageURL = "https://productimages.hepsiburada.net/s/108/550/110000052006351.jpg/format:webp"
 
+                }
             );
         }
     }
---
+++ ECommerceMVC.DataAccess/Migrations/20220503153556_init.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ECommerceMVC.DataAccess.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: true),
+                    Discount = table.Column<double>(type: "float", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: true),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Phone" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Computer" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Tablet" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "CreatedDate", "Description", "Discount", "ImageURL", "IsActive", "Name", "Price", "UpdateDate" },
+                values: new object[,]
+                {
+                    { 1, 1, null, "Awasome", 0.02, "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-2_large.jpg", true, "iPhone 11", 7000.0, null },
+                    { 2, 1, null, "Cool", 0.02, "https://productimages.hepsiburada.net/s/44/550/10769576230962.jpg/format:webp", true, "Samsung", 7000.0, null },
+                    { 3, 1, null, "Chang Ching Chong", 0.02, "https://productimages.hepsiburada.net/s/198/550/110000168630735.jpg/format:webp", true, "Xiomi", 7000.0, null },
+                    { 4, 2, null, "Interesting", 0.089999999999999997, "https://productimages.hepsiburada.net/s/44/222-222/10817626898482.jpg/format:webp", true, "MacBook Pro1", 20000.0, null },
+                    { 5, 2, null, "Ghost", 0.02, "https://productimages.hepsiburada.net/s/203/550/110000176911589.jpg/format:webp", true, "Casper", 7000.0, null },
+                    { 6, 2, null, "Canavar", 0.02, "https://productimages.hepsiburada.net/s/108/550/110000052006351.jpg/format:webp", true, "Monster", 7000.0, null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
