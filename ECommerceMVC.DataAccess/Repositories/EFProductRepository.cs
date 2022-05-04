a21e29f6d8a0301b8065f2652800e2f6e5c4fe28
TaylanCann <taylancanh@gmail.com>
2022-05-04 17:50:20 +0300

    Fixed EFProductRepository

---
+++ ECommerceMVC.DataAccess/Repositories/EFProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using ECommerceMVC.DataAccess.Data;
+using ECommerceMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.DataAccess.Repositories
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ECommerceDbContext context;
+        public EFProductRepository(ECommerceDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<int> Add(Product entity)
+        {
+            await context.Products.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity.Id;
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Product>> GetAllEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetEntityById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Product>> SearchProductsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ECommerceMVC.DataAccess/Repositories/IProductRepository.cs
+++ ECommerceMVC.DataAccess/Repositories/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerceMVC.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace ECommerceMVC.DataAccess.Repositories
 {
-    public interface IProductRepository
+    public interface IProductRepository : IRepository<Product>
     {
+        Task<IList<Product>> SearchProductsByName(string name);
     }
 }
