264811ac6aeaa549d89c6de154d7049f7bf62e3b
TaylanCann <taylancanh@gmail.com>
2022-05-04 16:25:53 +0300

    Add Repository Pattern

---
+++ ECommerceMVC.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.DataAccess.Repositories
+{
+    public interface IRepository
+    {
+    }
+}
