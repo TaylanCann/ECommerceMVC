8b655bc34436e596174431eb6ad882c5bae2626e
TaylanCann <taylancanh@gmail.com>
2022-05-04 12:59:18 +0300

    Add First Real Datas

--- ECommerceMVC.DataAccess/Data/ECommerceDbContext.cs
+++ ECommerceMVC.DataAccess/Data/ECommerceDbContext.cs
@@ -24,86 +24,84 @@ namespace ECommerceMVC.DataAccess.Data
                                           .HasForeignKey(p => p.CategoryId)
                                           .OnDelete(DeleteBehavior.NoAction);
 
-            modelBuilder.Entity<Category>().HasData(
+            #region FirstRealDatas
+            //modelBuilder.Entity<Category>().HasData(
 
-                new Category {  Id = 1, Name = "Phone", },
+            //    new Category {  Id = 1, Name = "Phone", },
 
-                new Category { Id = 2, Name = "Computer",},
+            //    new Category { Id = 2, Name = "Computer",},
 
-                new Category { Id = 3, Name = "Tablet" }
+            //    new Category { Id = 3, Name = "Tablet" }
 
-            );
+            //);
 
-            modelBuilder.Entity<Product>().HasData(
-                new Product
-                {
-                    Id = 1,
-                    Name = "iPhone 11",
-                    CategoryId = 1,
-                    Price = 7000,
-                    Discount = 0.02,
-                    Description = "Awasome",
-                    ImageURL = "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-2_large.jpg"
-                },
+            //modelBuilder.Entity<Product>().HasData(
+            //    new Product
+            //    {
+            //        Id = 1,
+            //        Name = "iPhone 11",
+            //        CategoryId = 1,
+            //        Price = 7000,
+            //        Discount = 0.02,
+            //        Description = "Awasome",
+            //        ImageURL = "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-2_large.jpg"
+            //    },
+            //    new Product
+            //    {
+            //        Id = 2,
+            //        Name = "Samsung",
+            //        CategoryId = 1,
+            //        Price = 7000,
+            //        Discount = 0.02,
+            //        Description = "Cool",
+            //        ImageURL = "https://productimages.hepsiburada.net/s/44/550/10769576230962.jpg/format:webp"
 
-                new Product
-                {
-                    Id = 2,
-                    Name = "Samsung",
-                    CategoryId = 1,
-                    Price = 7000,
-                    Discount = 0.02,
-                    Description = "Cool",
-                    ImageURL = "https://productimages.hepsiburada.net/s/44/550/10769576230962.jpg/format:webp"
+            //    },
+            //    new Product
+            //    {
+            //        Id = 3,
+            //        Name = "Xiomi",
+            //        CategoryId = 1,
+            //        Price = 7000,
+            //        Discount = 0.02,
+            //        Description = "Chang Ching Chong",
+            //        ImageURL = "https://productimages.hepsiburada.net/s/198/550/110000168630735.jpg/format:webp"
 
-                },
+            //    },
+            //    new Product
+            //    {
+            //        Id = 4,
+            //        Name = "MacBook Pro1",
+            //        CategoryId = 2,
+            //        Price = 20000,
+            //        Discount = 0.09,
+            //        Description = "Interesting",
+            //        ImageURL = "https://productimages.hepsiburada.net/s/44/222-222/10817626898482.jpg/format:webp"
+            //    },
+            //    new Product
+            //    {
+            //        Id = 5,
+            //        Name = "Casper",
+            //        CategoryId = 2,
+            //        Price = 7000,
+            //        Discount = 0.02,
+            //        Description = "Ghost",
+            //        ImageURL = "https://productimages.hepsiburada.net/s/203/550/110000176911589.jpg/format:webp"
 
-                new Product
-                {
-                    Id = 3,
-                    Name = "Xiomi",
-                    CategoryId = 1,
-                    Price = 7000,
-                    Discount = 0.02,
-                    Description = "Chang Ching Chong",
-                    ImageURL = "https://productimages.hepsiburada.net/s/198/550/110000168630735.jpg/format:webp"
+            //    },
+            //    new Product
+            //    {
+            //        Id = 6,
+            //        Name = "Monster",
+            //        CategoryId = 2,
+            //        Price = 7000,
+            //        Discount = 0.02,
+            //        Description = "Canavar",
+            //        ImageURL = "https://productimages.hepsiburada.net/s/108/550/110000052006351.jpg/format:webp"
 
-                },
-                new Product
-                {
-                    Id = 4,
-                    Name = "MacBook Pro1",
-                    CategoryId = 2,
-                    Price = 20000,
-                    Discount = 0.09,
-                    Description = "Interesting",
-                    ImageURL = "https://productimages.hepsiburada.net/s/44/222-222/10817626898482.jpg/format:webp"
-                },
-
-                new Product
-                {
-                    Id = 5,
-                    Name = "Casper",
-                    CategoryId = 2,
-                    Price = 7000,
-                    Discount = 0.02,
-                    Description = "Ghost",
-                    ImageURL = "https://productimages.hepsiburada.net/s/203/550/110000176911589.jpg/format:webp"
-
-                },
-
-                new Product
-                {
-                    Id = 6,
-                    Name = "Monster",
-                    CategoryId = 2,
-                    Price = 7000,
-                    Discount = 0.02,
-                    Description = "Canavar",
-                    ImageURL = "https://productimages.hepsiburada.net/s/108/550/110000052006351.jpg/format:webp"
-
-                }
-            );
+            //    }
+            //);
+            #endregion
         }
     }
 }
