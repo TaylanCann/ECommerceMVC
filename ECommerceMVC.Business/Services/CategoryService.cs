e0856ecd65143315fcbe03d9612c015fd8a1378a
TaylanCann <taylancanh@gmail.com>
2022-05-04 13:00:09 +0300

    Add Real Category Service

---
+++ ECommerceMVC.Business/Services/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceMVC.Business.IServices;
+using ECommerceMVC.DataAccess.Data;
+using ECommerceMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Business.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private ECommerceDbContext dbContext;
+        public CategoryService(ECommerceDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IList<Category> GetCategories()
+        {
+            return dbContext.Categories.ToList();
+        }
+    }
+}
