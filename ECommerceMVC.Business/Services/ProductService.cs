35ffb0c13e8f0dfcc1da4b5bf8bb7df5f8400625
TaylanCann <taylancanh@gmail.com>
2022-05-04 13:00:28 +0300

    Add Real Product Service

---
+++ ECommerceMVC.Business/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceMVC.DataAccess.Data;
+using ECommerceMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Business.Services
+{
+    public class ProductService : IProductService
+    {
+        private ECommerceDbContext dbContext;
+        public ProductService(ECommerceDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public ICollection<Product> GetProducts()
+        {
+            return dbContext.Products.ToList();
+        }
+    }
+}
