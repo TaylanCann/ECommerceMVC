faf2725b31bced8378b47de339ffffdddeba28bf
TaylanCann <taylancanh@gmail.com>
2022-05-05 13:34:46 +0300

    Add new item with select category

--- ECommerceMVC/Controllers/ProductsController.cs
+++ ECommerceMVC/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
-﻿using ECommerceMVC.Business.Services;
+﻿using ECommerceMVC.Business.IServices;
+using ECommerceMVC.Business.Services;
 using ECommerceMVC.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ECommerceMVC.Controllers
@@ -8,10 +12,12 @@ namespace ECommerceMVC.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService,ICategoryService categoryService)
         {
             this.productService = productService;
+            this.categoryService = categoryService;
         }
         public async Task<IActionResult> Index()
         {
@@ -22,6 +28,11 @@ namespace ECommerceMVC.Controllers
         [HttpGet]
         public IActionResult Create() 
         {
+            var selectedItems = new List<SelectListItem>();
+            categoryService.GetCategories().ToList().ForEach(cat => selectedItems.Add(new
+                SelectListItem
+            { Text = cat.Name, Value = cat.Id.ToString() }));
+            ViewBag.Categories = selectedItems;
             return View();
         }
 
