0696d03c4f29996b7b70cefd995136fa83c28f25
TaylanCann <taylancanh@gmail.com>
2022-05-07 20:55:36 +0300

    Add UserService

---
+++ ECommerceMVC.Business/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceMVC.Business.IServices;
+using ECommerceMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.Business.Services
+{
+    public class UserService : IUserService
+    {
+        public User ValidateUser(string UserName, string Password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
