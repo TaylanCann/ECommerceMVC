3dcf7f5c77ac06b587ec18f5cd16e2721a030bf1
TaylanCann <taylancanh@gmail.com>
2022-05-07 20:54:51 +0300

    Add Role

--- ECommerceMVC.Entities/User.cs
+++ ECommerceMVC.Entities/User.cs
@@ -14,5 +14,6 @@ namespace ECommerceMVC.Entities
         public string UserName { get; set; }
         public string Password { get; set; }
         public string EMail { get; set; }
+        public string Role { get; set; }
     }
 }
