c5b0a293380544958350a5a7f2ef71cf9bc3660e
TaylanCann <taylancanh@gmail.com>
2022-05-07 20:56:30 +0300

    Add EFUserRepository

---
+++ ECommerceMVC.DataAccess/Repositories/EFUserRepository.cs
@@ -0,0 +1,47 @@
+﻿using ECommerceMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.DataAccess.Repositories
+{
+    public class EFUserRepository : IUserRepository
+    {
+        public Task<int> Add(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User>> GetAllEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetEntityById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<User>> SearchUsersByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
