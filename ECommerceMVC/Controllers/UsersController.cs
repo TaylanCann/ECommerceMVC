e49d3d407d5150a33cd3a316d59a50b50fc16b68
TaylanCann <taylancanh@gmail.com>
2022-05-07 03:31:11 +0300

    Add UserController

---
+++ ECommerceMVC/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login() 
+        {
+            return View();
+        }
+    }
+}
