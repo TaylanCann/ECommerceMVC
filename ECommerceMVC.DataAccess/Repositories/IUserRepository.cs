70eb5271613416d9cb94f928978f12d2a898b752
TaylanCann <taylancanh@gmail.com>
2022-05-07 20:56:43 +0300

    Add IUserRepository

---
+++ ECommerceMVC.DataAccess/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMVC.DataAccess.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<IList<User>> SearchUsersByName(string name);
+
+    }
+}
