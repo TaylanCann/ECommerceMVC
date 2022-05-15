fb905f72a325de79df10d6fb1e1f8d0fac65e2b5
TaylanCann <taylancanh@gmail.com>
2022-05-15 22:20:33 +0300

    Add sessions

---
+++ ECommerceMVC/Extensions/SessionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ECommerceMVC.Extensions
+{
+    public static class SessionExtension
+    {
+        public static void SetJson(this ISession session,string key,object value) 
+        { 
+            session.SetJson(key,value); 
+        }
+    }
+}
