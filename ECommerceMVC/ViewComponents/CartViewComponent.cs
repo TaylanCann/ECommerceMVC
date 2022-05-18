3a9360d13fe026a25386bcd021885c7e6fd349e4
TaylanCann <taylancanh@gmail.com>
2022-05-18 14:33:11 +0300

    Added CartViewComponent

---
+++ ECommerceMVC/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceMVC.Extensions;
+using ECommerceMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            var collection = HttpContext.Session.GetJson<CartCollection>("basket");
+            return View(collection);
+        }
+    }
+}
