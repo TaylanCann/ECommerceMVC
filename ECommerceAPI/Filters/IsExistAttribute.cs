c73fe8e5dc0c96ecd260b976830732cfce4bb7f0
TaylanCann <taylancanh@gmail.com>
2022-05-25 01:21:07 +0300

    AddIsExistAttribute

---
+++ ECommerceAPI/Filters/IsExistAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ECommerceAPI.Filters
+{
+    public class IsExistAttribute : TypeFilterAttribute
+    {
+        public IsExistAttribute() : base(typeof(CheckExisting))
+        {
+
+        }
+    }
+}
